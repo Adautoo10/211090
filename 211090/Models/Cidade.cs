c979e87fa6fc7e7d405f61c533f616f5c508b069
Adautoo10 <adauto.facin05@gmail.com>
2022-10-05 11:32:00 -0300

    Adicionando Models

---
+++ 211090/Models/Cidade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace _211090.Models
+{
+    public class Cidade
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+
+        public string uf { get; set; }
+
+
+        public void Incluir()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+
+                Banco.Comando = new MySqlCommand("INSERT INTO cidades (nome, uf) VALUES (@nome, @uf)", Banco.Conexao);
+
+                Banco.Comando.Parameters.AddWithValue("@nome", nome);
+                Banco.Comando.Parameters.AddWithValue("@uf", uf);
+
+                Banco.Comando.ExecuteNonQuery();
+
+                Banco.FecharConexao();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
