1f206eeb58f20e5044ae76e0a0ca0843e72abf90
Adautoo10 <adauto.facin05@gmail.com>
2022-10-05 11:32:43 -0300

    Atualizando Projeto

---
+++ 211090/FrmMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using _211090.Views;
+
+
+namespace _211090
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+            Banco.CriarBanco();
+        }
+
+        private void cidadesToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+            FrmCidades form = new FrmCidades();
+            form.Show();
+
+        }
+    }
+}
