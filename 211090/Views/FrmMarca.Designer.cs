6982c53a990f864057721a33ca5e7d717797fa4e
Usuario <Usuario@CPD03-M05>
2022-11-09 12:04:31 -0300

    Corrigindo escrita

--- 211090/Views/FrmMarca.Designer.cs
+++ 211090/Views/FrmMarca.Designer.cs
@@ -155,9 +155,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(35, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 21;
-            this.label2.Text = "Cidade:";
+            this.label2.Text = "Marca:";
             // 
             // txtMarca
             // 
